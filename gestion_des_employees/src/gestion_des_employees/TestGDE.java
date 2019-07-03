7e2085239fd7a148878dac5e655a3f648489b607
Omar <Omar@DESKTOP-LGMPTC8>
2019-07-03 16:50:31 +0100

    commit testgde

--- gestion_des_employees/src/gestion_des_employees/TestGDE.java
+++ gestion_des_employees/src/gestion_des_employees/TestGDE.java
@@ -15,17 +15,17 @@ public class TestGDE {
 		e1.setNote(8.2);
 		e1.setService("dprt info");
 		e1.setDate_eval("10/9/2018");
-		e1.setHistorique_maladie("pas de maladie");
+		e1.setHistorique_maladie(" maladie");
 		
 		Employe e2 =new Employe();
-		e1.setNom("foulen");
-		e1.setPrenom("fouleni");
-		e1.setMatricule(0002);
-		e1.setSexe("homme");
-		e1.setNote(6.5);
-		e1.setService("dprt chimie");
-		e1.setDate_eval("15/9/2019");
-		e1.setHistorique_maladie("pas de maladie");
+		e2.setNom("foulen");
+		e2.setPrenom("fouleni");
+		e2.setMatricule(0002);
+		e2.setSexe("homme");
+		e2.setNote(6.5);
+		e2.setService("dprt chimie");
+		e2.setDate_eval("15/9/2019");
+		e2.setHistorique_maladie("pas de maladie");
 		
 		gde.addEmploye(e1);
 		gde.addEmploye(e2);
